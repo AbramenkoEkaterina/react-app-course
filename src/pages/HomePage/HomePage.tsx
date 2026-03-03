2e1a8e1427556d3a535929544485a9b973f35d05
Abramenko Ekaterina <katiatgn@yandex.ru>
2026-03-03 12:57:55 +0300

    useEffect, move bd, api_url

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,13 +1,33 @@
 import { QuestionCard } from '../../components/QuestionCard';
 import styles from './HomePage.module.css';
-import { mockQuestions } from '../../components/QuestionCard/QuestionCard';
+import { API_URL } from '../../constants';
+import { useEffect, useState } from 'react';
+import type { IQuestion } from '../../components/QuestionCard/types';
 
 export const HomePage = () => {
+  const [questions, setQuestions] = useState<IQuestion[]>([]);
+
+  const getQuestions = async (): Promise<void> => {
+    try {
+      const response = await fetch(`${API_URL}/react`);
+      const questions = await response.json();
+      setQuestions(questions);
+      console.log("questions", questions)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    getQuestions();
+  }, [])
+
   return (
     <>
-      {mockQuestions.map((card, index) => (
+      {questions.map((card, index) => (
         <QuestionCard card={card} key={index} />
       ))}
+
     </>
   );
 };
